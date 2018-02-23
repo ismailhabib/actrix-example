45a94a8ed15c10cc94b3482a769970d0ef386333
Habib <ismail.muhammad@mendix.com>
2018-02-23 16:13:15 +0100

    Remove unnecessary stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,19 @@ import * as React from "react";
 import "./App.css";
 import * as ioClient from "socket.io-client";
 import { ActorSystem, TypedActorRef } from "./Actor/ActorSystem";
-import { ClientActor, ClientActorPayload } from "./ClientActor/ClientActor";
 import { Chat } from "./Chat";
 
 const logo = require("./logo.svg");
 
-class App extends React.Component<{}, { message: string }> {
+class App extends React.Component<{}, {}> {
     constructor(props: {}) {
         super(props);
         this.state = { message: "no message" };
-    }
-    componentDidMount() {
-        // const socket = ioClient.connect("/ws");
-        // socket.on("greet", (message: string) => {
-        //     this.setState({ message: message });
-        // });
-        // const actorSystem = new ActorSystem();
-        // actorSystem.listenTo(socket);
-        // actorSystem.createActor("clientActor", ClientActor);
-        // const actorRef = actorSystem.findActor("clientActor");
-        // if (actorRef) {
-        //     actorRef.asTypedActorRef(ClientActor).putToMailbox(
-        //         "registerCallback",
-        //         payload => {
-        //             this.setState({ message: payload });
-        //         },
-        //         null
-        //     );
-        // }
     }
 
     render() {
         return (
             <div className="App">
-                Latest message: {this.state.message}
-                <textarea readOnly={true}>{this.state.message}</textarea>
-                <textarea />
-                <button>Post</button>
                 <Chat />
             </div>
         );
--- src/Server.ts
+++ src/Server.ts
@@ -3,34 +3,16 @@ import * as path from "path";
 import * as bodyParser from "body-parser";
 import * as socketIO from "socket.io";
 import { ActorSystem } from "./Actor/ActorSystem";
-import { ServerActor } from "./ServerActor/ServerActor";
 import { ChatServerActor } from "./ServerActor/ChatServerActor";
 
 const app = express();
 app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/ping", (req, res) => {
-    return res.send("pong");
-});
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../build", "index.html"));
-// });
 
 const server = app.listen(process.env.PORT || 8888);
 console.log("App is started");
 
 const io = socketIO(server);
 console.log("Web socket server is started");
-
-// const actorSystem = new ActorSystem();
-
-// actorSystem.createActor("serverActor", ServerActor);
-
-// io.of("/ws").on("connection", socket => {
-//     console.log("A connection has been started");
-//     actorSystem.listenTo(socket);
-// });
 
 const actorSystem = new ActorSystem("server");
 
